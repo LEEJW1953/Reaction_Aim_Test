c71a7a3758341a94436ff31c5edc9778cbb8cafb
LEEJW1953 <jiwonlee1953@naver.com>
2023-04-02 18:54:03 +0900

    feat: 메인메뉴 버튼 추가, 최고기록 저장 기능 추가

--- js/ReactionTest.js
+++ js/ReactionTest.js
@@ -5,6 +5,10 @@ const reactionTest = document.querySelector("#reactionTest");
 const progress = document.querySelector("#progress");
 const progressState = document.querySelector("#progressState");
 const progressBar = document.querySelector("#progressBar");
+const mainButton = document.querySelector("#mainButton");
+const RECORDS_KEY = "reaction_records";
+const records = document.querySelector("#records");
+const highRecords = document.querySelector("#highRecords");
 
 let currentGameNum = 0;
 let changeState;
@@ -82,14 +86,14 @@ function tooFast() {
 // 화면의 색이 바뀌었을 경우
 function clickNow() {
   clickTime = new Date();
+  const currentRecord = clickTime - startTime;
   reactionTest.classList.add("timeResult");
   currentGameNum += 1;
   progressState.innerText = `${currentGameNum} / ${testNumber}`;
   progressBar.value = parseInt((currentGameNum / testNumber) * 100);
-  reactionTest.innerText = `${currentGameNum}회 : ${
-    clickTime - startTime
-  }ms\n계속하려면 클릭하세요`;
-  resultTimes.push(clickTime - startTime);
+  reactionTest.innerText = `${currentGameNum}회 : ${currentRecord}ms\n계속하려면 클릭하세요`;
+  resultTimes.push(currentRecord);
+  saveRecords(currentRecord);
   reactionTest.classList.replace("clickNow", "startTest");
 }
 
@@ -104,11 +108,50 @@ function resultPage() {
   }ms\n${testNumber}회의 평균 반응속도는 ${avgTime}ms 입니다\n메인화면으로 돌아가려면 클릭하세요`;
   reactionTest.addEventListener("click", resetTest);
   progress.classList.add("hidden");
+  showRecords();
+}
+
+// local storage에 기록 저장
+function saveRecords(currentRecord) {
+  if (localStorage.getItem(RECORDS_KEY) === null) {
+    localStorage.setItem(RECORDS_KEY, "[]");
+  }
+  const recordArray = localStorage.getItem(RECORDS_KEY);
+  const highRecord = JSON.parse(recordArray);
+  highRecord.push(currentRecord);
+  highRecord.sort((a, b) => a - b);
+  if (highRecord.length > 10) highRecord.pop();
+  localStorage.setItem(RECORDS_KEY, JSON.stringify(highRecord));
+}
+
+// 테스트 종료 후 저장된 기록 상위 10개를 보여줌
+function showRecords() {
+  records.classList.remove("hidden");
+  const recordArray = localStorage.getItem(RECORDS_KEY);
+  const highRecord = JSON.parse(recordArray);
+  let result = "<ol>최고기록";
+  highRecord.forEach((c) => (result += `<li>${c}ms</li>`));
+  result += "</ol>";
+  highRecords.innerHTML = result;
+  resetRecords();
+}
+
+// 기록 초기화
+function resetRecords() {
+  const resetRecordButton = document.createElement("button");
+  resetRecordButton.innerText = "기록 초기화";
+  resetRecordButton.setAttribute("id", "resetRecord");
+  highRecords.appendChild(resetRecordButton);
+
+  resetRecordButton.addEventListener("click", () => {
+    localStorage.removeItem(RECORDS_KEY);
+  });
 }
 
 // 테스트를 초기화하고 메인 화면으로 돌아감
 function resetTest() {
   testStart.classList.remove("hidden");
+  progress.classList.add("hidden");
   reactionTest.classList.remove(
     "reactionTestBox",
     "startTest",
@@ -119,8 +162,11 @@ function resetTest() {
   currentGameNum = 0;
   resultTimes = [];
   progressBar.value = 0;
+  highRecords.innerHTML = "";
+  records.classList.add("hidden");
   reactionTest.removeEventListener("click", resetTest);
   reactionTest.removeEventListener("click", testClick);
 }
 
 reactionTestButton.addEventListener("click", startReactionTimeTest);
+mainButton.addEventListener("click", resetTest);
